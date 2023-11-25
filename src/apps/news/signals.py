57428c4f0e22d73b02ad6d89d650402b04a713ad
Josué Tamayo <josue.tamayo@cumplo.com>
2023-11-24 20:25:23 -0600

    feat(news): eliminar imagenes de la noticia

--- src/apps/news/signals.py
+++ src/apps/news/signals.py
@@ -10,10 +10,7 @@ from apps.news.models import News
 @receiver(pre_delete, sender=News)
 def delete_news_cover(sender, instance, **kwargs):
     # Elimina el archivo de imagen relacionado cuando se elimina la instancia de News
-    print('>> instance')
-    print(instance)
     if instance.cover:
-    	instance.cover.delete(save=True)
-        # image_path = instance.cover.path
-        # if os.path.isfile(image_path):
-        #     os.remove(image_path)
+        instance.cover.delete(save=True)
+        for image in instance.gallery.all():
+            image.delete()
